97ff408be4974913fd547af0d6fb2982ebb34e9d
mewcoder <1010953107@qq.com>
2022-05-03 22:34:37 +0800

    feat: mod flat

--- 01-javascript/test/05-array2.spec.js
+++ 01-javascript/test/05-array2.spec.js
@@ -1,4 +1,4 @@
-import { flat1, flat2, flat3 } from "../05-array2";
+import { flat1, flat2, flat3, flat4 } from "../05-array2";
 
 describe("05-array2", () => {
   it("flat1", () => {
@@ -21,6 +21,14 @@ describe("05-array2", () => {
     expect(flat3([1, 2, [3, 4]])).toEqual([1, 2, 3, 4]);
     expect(flat3([1, 2, [3, 4, [5, 6]]], 2)).toEqual([1, 2, 3, 4, 5, 6]);
     expect(flat3([1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]], Infinity)).toEqual([
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    ]);
+  });
+
+  it("flat4", () => {
+    expect(flat4([1, 2, [3, 4]])).toEqual([1, 2, 3, 4]);
+    expect(flat4([1, 2, [3, 4, [5, 6]]], 2)).toEqual([1, 2, 3, 4, 5, 6]);
+    expect(flat4([1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]], Infinity)).toEqual([
       1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
     ]);
   });
