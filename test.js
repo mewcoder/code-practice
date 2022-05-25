d1a99bc7936c3057be1dc1a976191ca2423260fb
mewcoder <1010953107@qq.com>
2022-05-25 22:58:35 +0800

    feat: 增加编译

--- test.js
+++ test.js
@@ -1,5 +1,11 @@
-const a = 'aaa';
+const list = [1, 2, 3, 4];
+function test1() {
+  list.forEach((item) => {
+    if (item == 3) {
+      return;
+    }
+    console.log(item);
+  });
+}
 
-(function () {
-  console.log(a)
-})();
+test1();
