154fb5bcada4ef1d0d521b29eeb674238f06789a
zhaodacheng5 <zhaodacheng5@hikvision.com.cn>
2022-06-06 11:26:30 +0800

    feat: 提交test

--- test.js
+++ test.js
@@ -1,5 +1,18 @@
-const a = 'aaa';
+const listToTree = (arr, id) => {
+  return arr
+    .filter(item => item.parentMenuId === id)
+    .map(c => ({ ...c, children: listToTree(arr, c.menuId) }));
+};
 
-(function () {
-  console.log(a)
-})();
+const deepClone = (obj = {}, map = new Map()) => {
+  if (typeof obj !== 'object') return obj;
+  if (map.has(obj)) return map.get(obj);
+  const result = Array.isArray(obj) ? [] : {};
+  map.set(obj, result);
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      result[key] = deepClone(obj[key], map);
+    }
+  }
+  return result;
+};
